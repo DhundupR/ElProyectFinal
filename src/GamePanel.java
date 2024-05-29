623b08f437d17aa2b52a5579a330d4ff213566dd
BT_1N3_33 <asd@ASDF>
2024-05-29 09:20:15 -0400

    stuff added

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,3 +1,120 @@
-public class GamePanel {
 
+
+
+import javax.imageio.IIOException;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Objects;
+
+
+public class GamePanel extends JPanel implements Runnable {
+    final int origTileSize = 16;
+    final int scale = 4;
+    final int tileSize = scale*origTileSize; // 64
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol;
+    final int screenHeight = tileSize * maxScreenRow
+
+    public Movement move = new Movement();
+
+
+
+    int playerX = 100; int playerY = 100; int playerSpeed= 10; //movement related
+    public Graphics2D g2;
+    public Graphics g;
+
+
+    double fps = 60;
+    Thread gameThread; //Keeps the image running and has uses later like reset screen and other stuff.
+
+
+    public TileManager manager = new TileManager(this);
+
+
+
+    public GamePanel(){
+        this.setPreferredSize(new Dimension(screenWidth,screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+        this.addKeyListener(move);
+        this.setFocusable(true);
+
+
+
+
+    }
+
+
+
+
+    public void paintComponent(Graphics g){
+
+
+        super.paintComponent(g); //parent class is gamePlanner
+        this.g = g;
+        g2 = (Graphics2D) g; //extension of graphic (has special functions)
+
+        manager.draw(g2);
+
+
+
+
+
+
+
+
+    }
+
+
+    public void startGame(){ //innate the thread
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+
+    public void update(){ //dont know why its opposite
+
+
+    }
+
+
+
+
+    @Override
+    public void run() {
+        double drawInter = 1000000000.0/fps;
+
+
+
+
+        long lastTime = System.nanoTime();
+        double timeCheck = 0;
+        long currentTime;
+
+
+        while(gameThread != null){ //keeps thing going until something occurs or breaks
+            currentTime = System.nanoTime();
+            timeCheck += (currentTime - lastTime)/ drawInter;
+            lastTime = currentTime;
+
+
+            if(timeCheck > 1){
+
+
+               // Add update code for main Player
+
+
+                repaint();
+                timeCheck--; //resets back to zero!
+            }
+
+
+        }
+
+
+    }
 }
---
+++ src/Movement.java
@@ -0,0 +1,89 @@
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Movement  implements KeyListener {
+
+    public boolean upPressed;
+    public boolean downPressed;
+    public boolean leftPressed;
+    public boolean rightPressed;
+    public boolean basicAttack1;
+    public boolean basicAttack2;
+    public boolean basicAttack3;
+    public boolean basicAttack4;
+
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        int code = e.getKeyCode();
+        if(code == KeyEvent.VK_X){
+            //use item
+
+        }
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int code = e.getKeyCode();
+        if(code == KeyEvent.VK_W){
+            upPressed = true;
+
+        }
+        if(code == KeyEvent.VK_A){
+            leftPressed = true;
+
+        }
+        if(code == KeyEvent.VK_S){
+            downPressed = true;
+
+        }if(code == KeyEvent.VK_D){
+
+            rightPressed = true;
+
+        }
+        if(code==KeyEvent.VK_1){
+            basicAttack1=true;
+        }
+        if(code==KeyEvent.VK_2){
+            basicAttack2=true;
+        }
+        if(code==KeyEvent.VK_3){
+            basicAttack3=true;
+        }
+        if(code==KeyEvent.VK_4){
+            basicAttack4=true;
+        }
+
+
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+        int code = e.getKeyCode();
+        if(code == KeyEvent.VK_W){
+            upPressed = false;
+        }
+        if(code == KeyEvent.VK_A){
+            leftPressed = false;
+
+        }
+        if(code == KeyEvent.VK_S){
+            downPressed = false;
+
+        }if(code == KeyEvent.VK_D){
+
+            rightPressed = false;
+
+        }
+
+
+
+
+    }
+
+
+}
